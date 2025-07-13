c00c9718148d22bab37fffc7cd50ea8c3ad9e7cd
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-12 20:36:29 -0700

    Add react-hot-toast for notifications and integrate it into login flow.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import AppRoutes from './routes/AppRoutes';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div>
       <AppRoutes />;
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          style: {
+            fontFamily: 'IRANYekan, sans-serif',
+            fontSize: '14px',
+          },
+        }}
+      />
     </div>
   );
 }
--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,18 +1,20 @@
 import { useMutation } from '@tanstack/react-query';
 import { loginUser } from '@/services/authService';
+import toast from 'react-hot-toast';
 
 export const useLogin = () => {
   const { mutate, status } = useMutation({
     mutationFn: loginUser,
     onSuccess: ({ access, refresh }) => {
+      toast.success('ورود با موفقیت انجام شد', { duration: 3000 });
       localStorage.setItem('access', access);
       localStorage.setItem('refresh', refresh);
     },
     onError: (error: Error) => {
       if (error.message.includes('401')) {
-        console.log('کاربری با این مشخصات وجود ندارد');
+        toast.error('کاربری با این مشخصات وجود ندارد', { duration: 3000 });
       } else {
-        console.log('خطا در برقراری ارتباط');
+        toast.error('خطا در برقراری ارتباط', { duration: 3000 });
       }
     },
   });
